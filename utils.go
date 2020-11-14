a6e50589c4b2d4da6e091d881ca3a05661ec90a0
Perelandric <perelandric@gmail.com>
2020-11-14 12:35:07 -0600

    improve pool names

--- utils.go
+++ utils.go
@@ -3,22 +3,22 @@ package freak
 import "sync"
 
 // TODO: Eventually have different pools for different sizes (to some limit)
-var respEndingFuncPool = sync.Pool{
+var wrapEndingFuncPool = sync.Pool{
 	New: func() interface{} {
 		return make([]func(*Response), 0, 1)
 	},
 }
 
-func getRespEndingFuncSlice() []func(*Response) {
-	return respEndingFuncPool.Get().([]func(*Response))
+func getWrapEndingFuncSlice() []func(*Response) {
+	return wrapEndingFuncPool.Get().([]func(*Response))
 }
 
 const endingFuncMaxCap = 2
 
-func returnRespEndingFuncSlice(s []func(*Response)) {
+func returnWrapEndingFuncSlice(s []func(*Response)) {
 	s = s[0:0:min(endingFuncMaxCap, cap(s))]
 	for i := range s {
 		s[i] = nil
 	}
-	respEndingFuncPool.Put(s)
+	wrapEndingFuncPool.Put(s)
 }
