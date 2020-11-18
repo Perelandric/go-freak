3a67d603c80ef7eaba3940f5be4edfc1b3e180c8
Perelandric <perelandric@gmail.com>
2020-11-17 19:39:55 -0600

    move the internal 'do' method to the Response

--- components.go
+++ components.go
@@ -1,7 +1,5 @@
 package freak
 
-import "reflect"
-
 type component struct {
 	markers           []*marker
 	htmlTail          []byte
@@ -35,91 +33,4 @@ func Wrapper(
 		preContent:  c1,
 		postContent: c2,
 	}
-}
-
-func (c *component) do(r *Response, dataI interface{}) {
-	if r.halt {
-		return
-	}
-
-	var callArgs = [2]reflect.Value{r.thisAsValue, reflect.ValueOf(dataI)}
-
-	var wrapperEndStack [][]func(*Response)
-	var endStackIndex = -1
-
-	if c.maxWrapperNesting != 0 {
-		wrapperEndStack = getWrapEndingSliceStack(c.maxWrapperNesting)
-		defer returnWrapEndingSliceStack(wrapperEndStack)
-	}
-
-	for i := 0; i < len(c.markers); i++ {
-		var m = c.markers[i]
-
-		r.buf = append(r.buf, m.htmlPrefix...)
-
-		switch m.kind {
-		case plainMarker:
-			m.fn.Call(callArgs[:])
-			if r.halt {
-				return
-			}
-
-		case wrapperStartMarker:
-			endStackIndex++
-
-			if endStackIndex >= len(wrapperEndStack) {
-				panic("unreachable")
-			}
-
-			var funcSlice = wrapperEndStack[endStackIndex]
-			r.wrapperEndingFuncs = funcSlice[0:0:cap(funcSlice)]
-
-			m.fn.Call(callArgs[:])
-
-			if r.halt {
-				return
-			}
-
-			// In case the end-funcs slice was grown beyond itw original capacity
-			wrapperEndStack[endStackIndex] = r.wrapperEndingFuncs
-
-			r.wrapperEndingFuncs = nil
-
-			if !r.skipping {
-				continue
-			}
-			r.skipping = false // reset
-
-			// We're skipping to the ending, so set `i` and `m`, and fallthrough so
-			// that we're not writing the htmlPrefix, since it's part of the content
-			i = int(m.wrapperEndIndex)
-			m = c.markers[i]
-			fallthrough
-
-		case wrapperEnd:
-			if len(wrapperEndStack) == 0 {
-				panic("unreachable")
-			}
-
-			var funcSlice = wrapperEndStack[endStackIndex]
-
-			for i := len(funcSlice) - 1; i != -1 && !r.halt; i-- {
-				funcSlice[i](r)
-				if r.halt {
-					return
-				}
-			}
-
-			endStackIndex--
-
-		default:
-			panic("unreachable")
-		}
-	}
-
-	if endStackIndex != -1 {
-		panic("unreachable")
-	}
-
-	r.buf = append(r.buf, c.htmlTail...)
 }
--- response.go
+++ response.go
@@ -24,6 +24,8 @@ type Response struct {
 	halt     bool
 }
 
+// TODO: Seems like ServeHTTP should not be 'component' because then
+// 			end users could invoke it.
 func (c *component) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	var resp = Response{
 		req:  r,
@@ -33,7 +35,7 @@ func (c *component) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	resp.thisAsValue = reflect.ValueOf(&resp)
 
-	c.do(&resp, &RouteData{})
+	resp.do(c, &RouteData{})
 
 	if resp.halt {
 		return
@@ -59,7 +61,7 @@ func (r *Response) WriteBytesNoEscape(b []byte) {
 }
 
 func (r *Response) LoadComponent(c *component, dataI interface{}) {
-	c.do(r, dataI)
+	r.do(c, dataI)
 }
 
 type WrapperResponse struct {
@@ -75,13 +77,100 @@ func (wr *WrapperResponse) LoadWrapper(w *wrapper, dataI interface{}) {
 	var temp = wr.r.wrapperEndingFuncs
 	wr.r.wrapperEndingFuncs = nil
 
-	w.preContent.do(wr.r, dataI)
+	wr.r.do(&w.preContent, dataI)
 
 	wr.r.wrapperEndingFuncs = append(temp, func(r *Response) {
-		w.postContent.do(r, dataI)
+		r.do(&w.postContent, dataI)
 	})
 }
 
 func (wr *WrapperResponse) Skip() {
 	wr.r.skipping = true
+}
+
+func (r *Response) do(c *component, dataI interface{}) {
+	if r.halt {
+		return
+	}
+
+	var callArgs = [2]reflect.Value{r.thisAsValue, reflect.ValueOf(dataI)}
+
+	var wrapperEndStack [][]func(*Response)
+	var endStackIndex = -1
+
+	if c.maxWrapperNesting != 0 {
+		wrapperEndStack = getWrapEndingSliceStack(c.maxWrapperNesting)
+		defer returnWrapEndingSliceStack(wrapperEndStack)
+	}
+
+	for i := 0; i < len(c.markers); i++ {
+		var m = c.markers[i]
+
+		r.buf = append(r.buf, m.htmlPrefix...)
+
+		switch m.kind {
+		case plainMarker:
+			m.fn.Call(callArgs[:])
+			if r.halt {
+				return
+			}
+
+		case wrapperStartMarker:
+			endStackIndex++
+
+			if endStackIndex >= len(wrapperEndStack) {
+				panic("unreachable")
+			}
+
+			var funcSlice = wrapperEndStack[endStackIndex]
+			r.wrapperEndingFuncs = funcSlice[0:0:cap(funcSlice)]
+
+			m.fn.Call(callArgs[:])
+
+			if r.halt {
+				return
+			}
+
+			// In case the end-funcs slice was grown beyond itw original capacity
+			wrapperEndStack[endStackIndex] = r.wrapperEndingFuncs
+
+			r.wrapperEndingFuncs = nil
+
+			if !r.skipping {
+				continue
+			}
+			r.skipping = false // reset
+
+			// We're skipping to the ending, so set `i` and `m`, and fallthrough so
+			// that we're not writing the htmlPrefix, since it's part of the content
+			i = int(m.wrapperEndIndex)
+			m = c.markers[i]
+			fallthrough
+
+		case wrapperEnd:
+			if len(wrapperEndStack) == 0 {
+				panic("unreachable")
+			}
+
+			var funcSlice = wrapperEndStack[endStackIndex]
+
+			for i := len(funcSlice) - 1; i != -1 && !r.halt; i-- {
+				funcSlice[i](r)
+				if r.halt {
+					return
+				}
+			}
+
+			endStackIndex--
+
+		default:
+			panic("unreachable")
+		}
+	}
+
+	if endStackIndex != -1 {
+		panic("unreachable")
+	}
+
+	r.buf = append(r.buf, c.htmlTail...)
 }
