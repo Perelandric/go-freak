eccbedca5a78920151d8cd2bbe6e40d98cd39c25
Perelandric <perelandric@gmail.com>
2020-11-19 22:51:58 -0600

    assign missing reflect.Value to Response

--- response.go
+++ response.go
@@ -74,10 +74,12 @@ type Response struct {
 func newResponse(compressionLevel, bufMaxSize int) *Response {
 	gz, _ := gzip.NewWriterLevel(nil, compressionLevel)
 
-	return &Response{
+	var r = Response{
 		gzip: *gz,
 		buf:  *bytes.NewBuffer(make([]byte, 0, bufMaxSize)),
 	}
+	r.thisAsValue = reflect.ValueOf(r)
+	return &r
 }
 
 func (r *Response) WriteBytes(b []byte) {
