fcb411b3d889e741c603b9431eb7aa34f6cf71a6
Perelandric <perelandric@gmail.com>
2020-11-21 17:27:28 -0600

    add clarifying comments

--- caches.go
+++ caches.go
@@ -7,18 +7,40 @@ import (
 
 /*
 
-TODO: No strings are being cached, so go back and enable this.
+Now that all static []byte are consolidated into a contiguous block of memory,
+it would be good to try to find redundant patterns and consolidate those too.
 
-It reuses strings references to eliminate redundency in memory.
+To do this, we would need to maintain the original slices, or at least their
+start and end points in the monolithic []byte. Then for each slice, search for
+a match somewhere. When one is found, replace the []byte in the component using
+the pointer, and then block out those bytes as not being available for matches.
+
+This should have the effect of consolidating down the a smaller subset. I don't know
+if this is the most computationally efficient, or if it will yield the smallest result,
+but it should be better than nothing.
+
+In the end, we should have a more compact block of memory.
+
+Longest slices should be searched first.
+
+I don't know if the "removed" slices should be removed immediately. Could be that there
+will be some match that will start inside the more dense area, but extend out to where
+an old, removed slice would have started.
+
+Maybe just focus the searches first on the overlapping areas. To do this we would need
+to keep track of those areas. Maybe also track how many overlaps there are, so that new
+searches can always start in proximity of the most dense areas.
+
+A separate []uint16 could maybe be used, where each index corresponds to a byte index
+in the monolithic []byte, and it keeps track of how many overlapping slices there were
+for each byte.
 
 */
 
 var stringCache []*[]byte
 var stringCacheMux sync.Mutex
 
-// TODO: One thing I co
-
-// Inserts the string pointer in length-order
+// Inserts the []byte pointer in length-order
 func stringCacheInsert(buf *[]byte) {
 	var bufLen = len(*buf)
 	if bufLen == 0 {
@@ -33,8 +55,8 @@ func stringCacheInsert(buf *[]byte) {
 	stringCacheMux.Unlock()
 }
 
-// Is run after all Prototypes have been initialized.
-// Perhaps run again as new Prototypes are created if allowed during runtime.
+// Is run after all components have been initialized.
+// Perhaps run again as new component are created if allowed during runtime.
 func locateSubstrings() {
 	stringCacheMux.Lock() // Locking is probably not necessary, but doesn't hurt.
 	defer stringCacheMux.Unlock()
@@ -44,23 +66,21 @@ func locateSubstrings() {
 		return len(*stringCache[i]) < len(*stringCache[j])
 	})
 
-	// TODO: After all static HTML data has been received, should I put it into
-	//		one monolithic []byte, keep indices of where each one started, and then
-	//		take subslices from that single []byte? That would align a good bit of
-	//		memory.
-	//  If I precalculate the sums of the slices, I can pre-allocate and then safely
-	//   take pointers into the momo-slice.
-
+	// Calculate the total length needed for all static bytes
 	var totalLen = 0
 	for _, b := range stringCache {
 		totalLen += len(*b)
 	}
 
+	// Create a single byte slice to hold all static bytes
 	var monolithic = make([]byte, 0, totalLen)
 	//	var indexPairs = make([]struct{
 	//    start, end int, ptr *[]byte,
 	//  }, 0, len(stringCache))
 
+	// Copy all static bytes into the monolithic []byte, and use the given
+	// pointers to replace the existing slices with the correct subslice
+	// of the monolithic slice
 	for _, b := range stringCache {
 		var currStartIdx = len(monolithic)
 
